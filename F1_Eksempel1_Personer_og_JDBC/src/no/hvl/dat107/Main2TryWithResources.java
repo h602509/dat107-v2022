62e9663bd37266b8a4bc8894f79340f19cb7b19d
Lars-Petter Helland <lars-petter.helland@hvl.no>
2022-03-09 09:47:29 +0100

    Eksempler til F1 lagt ut før timen

---
+++ F1_Eksempel1_Personer_og_JDBC/src/no/hvl/dat107/Main2TryWithResources.java
@@ -0,0 +1,46 @@
+package no.hvl.dat107;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main2TryWithResources {
+
+	static final String JDBC_DRIVER = "org.postgresql.Driver";
+	
+//	static final String DB_URL = "jdbc:postgresql://localhost:5432/dat107v22";
+//	static final String USER = "postgres";
+//	static final String PASS = Passwords.LOCALHOST_PASSWORD;
+	
+ 	static final String DB_URL = "jdbc:postgresql://ider-database.westeurope.cloudapp.azure.com:5432/lph";
+	static final String USER = "lph";
+	static final String PASS = Passwords.AZURE_PASSWORD;
+
+	public static void main(String[] args) throws ClassNotFoundException {
+
+		Class.forName(JDBC_DRIVER);
+
+		System.out.println("Kobler til database...");
+
+		String sql = "SELECT id, navn FROM forelesning1.person";
+
+		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String navn = rs.getString("navn");
+
+				System.out.print("ID: " + id);
+				System.out.println(", Navn: " + navn);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Ferdig!");
+	}
+
+}
---
+++ F1_Eksempel1_Personer_og_JDBC/src/no/hvl/dat107/Main3UsingPersonObjects.java
@@ -0,0 +1,56 @@
+package no.hvl.dat107;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main3UsingPersonObjects {
+
+	static final String JDBC_DRIVER = "org.postgresql.Driver";
+	
+//	static final String DB_URL = "jdbc:postgresql://localhost:5432/dat107v22";
+//	static final String USER = "postgres";
+//	static final String PASS = Passwords.LOCALHOST_PASSWORD;
+	
+ 	static final String DB_URL = "jdbc:postgresql://ider-database.westeurope.cloudapp.azure.com:5432/lph";
+	static final String USER = "lph";
+	static final String PASS = Passwords.AZURE_PASSWORD;
+
+	public static void main(String[] args) throws ClassNotFoundException {
+
+		Class.forName(JDBC_DRIVER);
+
+		System.out.println("Kobler til database...");
+
+		String sql = "SELECT id, navn FROM forelesning1.person";
+
+		List<Person> personer = new ArrayList<>();
+		
+		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+
+			while (rs.next()) {
+				Person p = new Person();
+				p.setId(rs.getInt("id"));
+				p.setNavn(rs.getString("navn"));
+
+				personer.add(p);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		for (Person p : personer) {
+			System.out.print("Id: " + p.getId());
+			System.out.println(", Navn: " + p.getNavn());
+		}
+		
+		System.out.println("Ferdig!");
+	}
+
+}
